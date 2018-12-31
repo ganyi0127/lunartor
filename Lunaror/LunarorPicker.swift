e8b5d4c61347211255e33ba7418bfc640cd64325
ganyi <283925583@qq.com>
2018-12-31 15:36:10 +0800

    time 15:36

---
+++ Lunaror/LunarorPicker.swift
@@ -0,0 +1,31 @@
+//
+//  LunarorPicker.swift
+//  Lunaror
+//
+//  Created by YiGan on 2018/12/31.
+//  Copyright © 2018 YiGan. All rights reserved.
+//
+
+import Foundation
+class LunarorPicker: UIView {
+    
+    init() {
+        let frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+        super.init(frame: frame)
+        
+        config()
+        createContents()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func config() {
+        
+    }
+    
+    private func createContents() {
+        
+    }
+}
